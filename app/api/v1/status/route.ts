fcbe0582811c25af83a18b9f2e08df75fc6d792e
Adones Balica <adonesbalica@gmail.com>
2026-09-25 02:30:47 -0300

    feat: enhance API status endpoint to include database metrics and update environment configuration

--- app/api/v1/status/route.ts
+++ app/api/v1/status/route.ts
@@ -1,10 +1,40 @@
 import database from "@/infra/database";
 
 export async function GET(request: Request) {
-  const result = await database.query("SELECT 1 + 1 as SUM;");
-  console.log(result.rows);
-  return new Response(JSON.stringify({ status: "OK" }), {
-    status: 200,
-    headers: { "Content-Type": "application/json" },
+  const updatedAt = new Date().toISOString();
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
   });
+
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
+  return new Response(
+    JSON.stringify({
+      updated_at: updatedAt,
+      dependecies: {
+        database: {
+          version: databaseVersionValue,
+          max_connections: parseInt(databaseMaxConnectionValue),
+          opened_connections: databaseOpenedConnectionsValue,
+        },
+      },
+    }),
+    {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    },
+  );
 }
--- infra/database.ts
+++ infra/database.ts
@@ -1,19 +1,34 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
+  let client;
+  try {
+    client = await getNewClient();
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.log(error);
+  } finally {
+    await client?.end();
+  }
+}
+
+async function getNewClient() {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
-    port: 5432,
+    port: parseInt(process.env.POSTGRES_PORT),
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
+
   await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
-  return result;
+  return client;
 }
 
-export default {
-  query: query,
+const database = {
+  query,
+  getNewClient,
 };
+
+export default database;
